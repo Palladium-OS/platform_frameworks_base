6d0f2de0a339628a012d4f5538c4ff349904924a
Alexander Martinz <alex@amartinz.at>
2017-08-25 12:01:11 +0200

    BatteryService: Add support for OEM fast charger detection
    Allows to indicate, whether a device is charged using a
    proprietary OEM fast charge solution, which write their status
    to sysfs.
    
    The OEM fast charge detection tries to be as generic as possible and is
    configured via overlays.
    
    Path to sysfs to read status
      - core/res/res/values/custom_config.xml
        - config_oemFastChargerStatusPath
    Value expected from read status (Defaults to "1" if not specified)
      - core/res/res/values/custom_config.xml
        - config_oemFastChargerStatusValue
    
    Change-Id: I6f3598a5a6a3efc76553261d2cf73094170d4110
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
+++ packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
@@ -28,6 +28,7 @@ import static android.os.BatteryManager.EXTRA_TEMPERATURE;
 import static android.os.BatteryManager.EXTRA_PLUGGED;
 import static android.os.BatteryManager.EXTRA_PRESENT;
 import static android.os.BatteryManager.EXTRA_STATUS;
+import static android.os.BatteryManager.EXTRA_OEM_FAST_CHARGER;
 
 import android.content.Context;
 import android.content.Intent;
@@ -54,12 +55,13 @@ public class BatteryStatus {
     public final int maxChargingCurrent;
     public final int maxChargingVoltage;
     public final int maxChargingWattage;
+    public final boolean oemFastChargeStatus;
     public final boolean present;
 
     public final float temperature;
 
     public BatteryStatus(int status, int level, int plugged, int health,
-            int maxChargingWattage, boolean present,
+            int maxChargingWattage,boolean oemFastChargeStatus,boolean present,
             int maxChargingCurrent, int maxChargingVoltage,
             float temperature) {
         this.status = status;
@@ -69,6 +71,7 @@ public class BatteryStatus {
         this.maxChargingCurrent = maxChargingCurrent;
         this.maxChargingVoltage = maxChargingVoltage;
         this.maxChargingWattage = maxChargingWattage;
+        this.oemFastChargeStatus = oemFastChargeStatus;
         this.present = present;
         this.temperature = temperature;
     }
@@ -78,6 +81,7 @@ public class BatteryStatus {
         plugged = batteryChangedIntent.getIntExtra(EXTRA_PLUGGED, 0);
         level = batteryChangedIntent.getIntExtra(EXTRA_LEVEL, 0);
         health = batteryChangedIntent.getIntExtra(EXTRA_HEALTH, BATTERY_HEALTH_UNKNOWN);
+        oemFastChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_OEM_FAST_CHARGER, false);
         present = batteryChangedIntent.getBooleanExtra(EXTRA_PRESENT, true);
         temperature = batteryChangedIntent.getIntExtra(EXTRA_TEMPERATURE, -1);
 
@@ -158,6 +162,9 @@ public class BatteryStatus {
      * @return the charing speed
      */
     public final int getChargingSpeed(Context context) {
+        if (oemFastChargeStatus) {
+                return CHARGING_FAST;
+        }
         final int slowThreshold = context.getResources().getInteger(
                 R.integer.config_chargingSlowlyThreshold);
         final int fastThreshold = context.getResources().getInteger(
