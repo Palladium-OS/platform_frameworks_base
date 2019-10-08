81e606fae9b3af19aca01a2ba40eecc0c23f38bf
Pranav Vashi <neobuddy89@gmail.com>
2019-10-08 22:59:24 +0530

    base: Add tunables for navbar layout [1/2]
    idoybh:
    * Back it up properly
    * There is no use in it being under secure, system it is
    * Get rid of useless global vars
    * Use existing settings observer instead of tunables
    * Rewrote observing to match A12's way
    
    Change-Id: I95c7427312400540f840c48b9456e0fe71f74e5c
    
    also a squash of:
    
    base: Make a proper validator for navbar layouts
    
    Change-Id: Ia4a8ad42f8fcc1aeeaf1d4b42a5fda98aab2e42f
    
    NavigationBarInflaterView: Apply custom layouts on boot
    
    Change-Id: I0b9f85198b705c3f8140097c18988069bd965997
    
    NavigationBarInfalterView: Don't apply a custom layout when using gestural
    
    This causes gestural navbar to be gone and causes various other issues
    when it happens
    
    Change-Id: I56cfec66d551fb5bdcf37e82cfd91588a52cd988
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -228,5 +228,26 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.SCREEN_FLASH_NOTIFICATION, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.SCREEN_FLASH_NOTIFICATION_COLOR, ANY_INTEGER_VALIDATOR);
         VALIDATORS.put(System.NAVIGATION_BAR_INVERSE, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(System.NAVBAR_LAYOUT_VIEWS,
+                new Validator() {
+                    @Override
+                    public boolean validate(String value) {
+                        if (value.equals("default")) return true;
+                        int scCount = value.length() - value.replace(";", "").length();
+                        if (scCount != 2) return false;
+                        value = value.replace(";", ",");
+                        String[] args = value.split(",", 0);
+                        for (String str : args) {
+                            if (!str.equals("left") &&
+                                !str.equals("right") &&
+                                !str.equals("back") &&
+                                !str.equals("home") &&
+                                !str.equals("recent") &&
+                                !str.equals("space"))
+                                return false;
+                        }
+                        return true;
+                    }
+                });
     }
 }
--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
@@ -18,6 +18,7 @@ package com.android.systemui.navigationbar;
 
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_3BUTTON;
+import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_GESTURAL;
 
 import android.annotation.Nullable;
 import android.content.Context;
@@ -109,6 +110,7 @@ public class NavigationBarInflaterView extends FrameLayout {
     @VisibleForTesting
     SparseArray<ButtonDispatcher> mButtonDispatchers;
     private String mCurrentLayout;
+    private String mCustomLayout;
 
     private View mLastPortrait;
     private View mLastLandscape;
@@ -116,6 +118,7 @@ public class NavigationBarInflaterView extends FrameLayout {
     private boolean mIsVertical;
     private boolean mAlternativeOrder;
     private boolean mNavBarLayoutInverse;
+    private boolean mUsingCustomLayout;
 
     private OverviewProxyService mOverviewProxyService;
     private int mNavBarMode = NAV_BAR_MODE_3BUTTON;
@@ -129,6 +132,8 @@ public class NavigationBarInflaterView extends FrameLayout {
         final NavigationModeController controller = Dependency.get(NavigationModeController.class);
         mNavBarMode = controller.addListener(this);
         mNavBarLayoutInverse = controller.shouldInvertNavBarLayout();
+        mCustomLayout = controller.getCustomNavbarLayout();
+        mUsingCustomLayout = mCustomLayout != null && !mCustomLayout.equals("default");
         updateLayoutInversion();
     }
 
@@ -147,6 +152,7 @@ public class NavigationBarInflaterView extends FrameLayout {
         inflateChildren();
         clearViews();
         inflateLayout(getDefaultLayout());
+        onNavBarCustomLayoutChanged(mCustomLayout);
     }
 
     private void inflateChildren() {
@@ -171,6 +177,8 @@ public class NavigationBarInflaterView extends FrameLayout {
 
     private void onNavigationModeChanged(int mode) {
         mNavBarMode = mode;
+        onLikelyDefaultLayoutChange();
+        onNavBarCustomLayoutChanged(mCustomLayout);
     }
 
     @Override
@@ -194,6 +202,17 @@ public class NavigationBarInflaterView extends FrameLayout {
     }
 
     @Override
+    public void onNavBarCustomLayoutChanged(String layout) {
+        if (mNavBarMode == NAV_BAR_MODE_GESTURAL) return;
+        if (mCurrentLayout == null || !mCurrentLayout.equals(layout)) {
+            mUsingCustomLayout = layout != null && !layout.equals("default");
+            mCustomLayout = layout;
+            clearViews();
+            inflateLayout(mUsingCustomLayout ? layout : getDefaultLayout());
+        }
+    }
+
+    @Override
     protected void onDetachedFromWindow() {
         Dependency.get(NavigationModeController.class).removeListener(mListener);
         super.onDetachedFromWindow();
@@ -206,6 +225,9 @@ public class NavigationBarInflaterView extends FrameLayout {
     }
 
     public void onLikelyDefaultLayoutChange() {
+        // Don't override custom layouts unless we're using full gestures
+        if (mUsingCustomLayout && mNavBarMode != NAV_BAR_MODE_GESTURAL) return;
+
         // Reevaluate new layout
         final String newValue = getDefaultLayout();
         if (!Objects.equals(mCurrentLayout, newValue)) {
