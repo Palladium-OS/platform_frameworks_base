54bf7489e25422b067f2dbafd1eb95c6e28fd39a
Alex Cruz <alex@dirtyunicorns.com>
2019-11-12 00:47:34 +0100

    QS tile titles visibility [1/2]
    ----
    abc ezio84:
    don't call textVisibility on each handleStateChanged
    but expose it through TileLayout mRecords and call it only when
    changing the settings
    
    fix also title visibility not being refreshed after a
    configuration change
    ----
    
    Pretty simple mod to remove tile titles if and when the titles become
    something that you don't want. A good case for this is if you set 7 or more
    columns across, the titles move down to 2 lines and is just looks awful IMO
    
    Before - https://i.imgur.com/G3ICaqb.png
    After - https://i.imgur.com/Hb0J1XW.png
    
    PS2 - Thanks maxwen for the know how to adjust the gap when titles
    are removed
    
    Change-Id: I46fa9ebb01bd36f6589b0c6231a80dbc87fe1ca4

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2092,6 +2092,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_DATAUSAGE),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_TILE_TITLE_VISIBILITY),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -2107,8 +2110,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(Settings.System.QS_ROWS_PORTRAIT)) ||
                     uri.equals(Settings.System.getUriFor(Settings.System.QS_ROWS_LANDSCAPE)) ||
                     uri.equals(Settings.System.getUriFor(Settings.System.QS_COLUMNS_PORTRAIT)) ||
-                    uri.equals(Settings.System.getUriFor(Settings.System.QS_COLUMNS_LANDSCAPE))) {
-                setQsRowsColumns();
+                    uri.equals(Settings.System.getUriFor(Settings.System.QS_COLUMNS_LANDSCAPE)) ||
+                    uri.equals(Settings.System.getUriFor(Settings.System.QS_TILE_TITLE_VISIBILITY))) {
+                updateQsPanelResources();
             } else if (uri.equals(Settings.System.getUriFor(Settings.System.QS_TILE_STYLE))) {
                 stockTileStyle();
                 updateTileStyle();
@@ -2118,7 +2122,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         public void update() {
             setFpToDismissNotifications();
-            setQsRowsColumns();
+            updateQsPanelResources();
             setSBSleepGesture();
             setHeadsUpStoplist();
             setHeadsUpBlacklist();
@@ -2136,7 +2140,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             mNotificationShadeWindowViewController.updateSettings();
     }
 
-    private void setQsRowsColumns() {
+    private void updateQsPanelResources() {
         if (mQSPanel != null) {
             mQSPanel.updateResources();
         }
