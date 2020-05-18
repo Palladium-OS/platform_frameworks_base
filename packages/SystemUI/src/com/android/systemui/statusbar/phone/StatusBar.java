d0e5228eebf6d06925ff725827ce40228ce5a6e2
rituj <ritujbeniwal@gmail.com>
2020-05-18 12:23:21 +0530

    BiometricPrompt: Automatically add padding to incorporate FOD
    This effectively hides the default icon in the traditional
    biometrics prompt, so that only FOD icon is displayed.
    
    Signed-off-by: Rituj Beniwal <ritujbeniwal@gmail.com>
    Change-Id: I20b250ac59f0631e77356986a2d935d97842f9bb

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5435,4 +5435,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
         return null;
     }
+
+    public int getFodHeight(boolean includeDecor) {
+        return mFODCircleViewImpl.getHeight(includeDecor);
+    }
 }
