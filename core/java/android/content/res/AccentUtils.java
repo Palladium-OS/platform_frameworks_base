f61292a195ac3fbf7de16f29fa5ef283cdfa644f
SuperDroidBond <superdroidbond@yahoo.com>
2020-05-28 17:48:33 +0530

    RGB Accenter: [SQUASH] add more attrs for color changes
    Co-authored-by: AshutoshSundresh <ashutoshsundresh@gmail.com>
    Co-authored-by: spkal01 <kalligeross@gmail.com>
    Change-Id: Ia8286fd0727196927adca15ec5c7e0d67bb95f46
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

--- core/java/android/content/res/AccentUtils.java
+++ core/java/android/content/res/AccentUtils.java
@@ -22,6 +22,22 @@ public class AccentUtils {
                     "accent_device_default_dark",
                     "material_pixel_blue_dark",
                     "material_pixel_blue_bright",
+                    "colorAccent",
+                    "alert_dialog_color_accent_light",
+                    "alert_dialog_color_accent_dark",
+                    "lockscreen_clock_accent_color",
+                    "dismiss_all_icon_color",
+                    "material_pixel_blue_dark",
+                    "material_pixel_blue_bright",
+                    "omni_color5",
+                    "omni_color4",
+                    "oneplus_accent_color",
+                    "oneplus_accent_text_color",
+                    "dialer_theme_color",
+                    "dialer_theme_color_dark",
+                    "dialer_theme_color_20pct",
+                    "settings_accent_color",
+                    "settingsHeaderColor",
                     "gradient_start"));
 
     private static ArrayList<String> systemuiResources = new ArrayList<>(
