b12a810ec2f593c4b8e4c024b93aff20a06c020f
SKULSHADY <SKULSHADY@users.noreply.github.com>
2020-11-22 20:41:33 +0000

    base: Allow to wake the screen instead of pulsing [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5819,6 +5819,12 @@ public final class Settings {
         public static final String NOTIFICATION_PULSE_DURATION = "ambient_notification_light_duration";
 
         /**
+         * Custom Ambient raise gesture
+         * @hide
+         */
+        public static final String CUSTOM_AMBIENT_RAISE_GESTURE = "custom_ambient_raise_gesture";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
