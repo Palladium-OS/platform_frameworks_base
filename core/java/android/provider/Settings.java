8af24c5c19ee585c6d221ffeb4c2aeddd01b1729
00day0 <therandomuser11@gmail.com>
2020-12-26 14:40:11 +0300

    FODCircleView: Recognizing fingerprint animation [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5840,6 +5840,12 @@ public final class Settings {
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
         
         /**
+         * FOD recognizing animation
+         * @hide
+         */
+        public static final String FOD_RECOGNIZING_ANIMATION = "fod_recognizing_animation";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
