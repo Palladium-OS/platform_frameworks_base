58d25274a33b09c93b46a14c8dc0c7ac23032724
Ido Ben-Hur <idoybh2@gmail.com>
2021-05-13 18:48:20 +0300

    Screenrecord: Add an option to record for longer
    Unlimit time-wise. Only limit file size to 15GiB
    Change-Id: I85fe7ab5a0b019a34403f707f24566e627c65342
    
    Change-Id: Ia16045dde478f65d1bee4a5167684c403f018906

--- packages/SystemUI/src/com/android/systemui/screenrecord/RecordingService.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/RecordingService.java
@@ -73,6 +73,7 @@ public class RecordingService extends Service implements MediaRecorder.OnInfoLis
     private static final String EXTRA_AUDIO_SOURCE = "extra_useAudio";
     private static final String EXTRA_SHOW_STOP_DOT = "extra_showStopDot";
     private static final String EXTRA_LOW_QUALITY = "extra_lowQuality";
+    private static final String EXTRA_LONGER_DURATION = "extra_longerDuration";
 
     private static final String ACTION_START = "com.android.systemui.screenrecord.START";
     private static final String ACTION_STOP = "com.android.systemui.screenrecord.STOP";
@@ -92,6 +93,7 @@ public class RecordingService extends Service implements MediaRecorder.OnInfoLis
     private final UserContextProvider mUserContextTracker;
 
     private boolean mLowQuality;
+    private boolean mLongerDuration;
     private boolean mShowStopDot;
     private boolean mIsDotAtRight;
     private boolean mDotShowing;
@@ -121,13 +123,14 @@ public class RecordingService extends Service implements MediaRecorder.OnInfoLis
      * @param audioSource   The ordinal value of the audio source
      *                      {@link com.android.systemui.screenrecord.ScreenRecordingAudioSource}
      */
-    public static Intent getStartIntent(Context context, int resultCode, int audioSource, boolean showStopDot, boolean lowQuality) {
+    public static Intent getStartIntent(Context context, int resultCode, int audioSource, boolean showStopDot, boolean lowQuality, boolean longerDuration) {
         return new Intent(context, RecordingService.class)
                 .setAction(ACTION_START)
                 .putExtra(EXTRA_RESULT_CODE, resultCode)
                 .putExtra(EXTRA_AUDIO_SOURCE, audioSource)
                 .putExtra(EXTRA_SHOW_STOP_DOT, showStopDot)
-                .putExtra(EXTRA_LOW_QUALITY, lowQuality);
+                .putExtra(EXTRA_LOW_QUALITY, lowQuality)
+                .putExtra(EXTRA_LONGER_DURATION, longerDuration);
     }
 
     @Override
@@ -147,6 +150,7 @@ public class RecordingService extends Service implements MediaRecorder.OnInfoLis
                 Log.d(TAG, "recording with audio source" + mAudioSource);
                 mShowStopDot = intent.getBooleanExtra(EXTRA_SHOW_STOP_DOT, false);
                 mLowQuality = intent.getBooleanExtra(EXTRA_LOW_QUALITY, false);
+                mLongerDuration = intent.getBooleanExtra(EXTRA_LONGER_DURATION, false);
 
                 setStopDotVisible(mShowStopDot);
 
@@ -157,6 +161,7 @@ public class RecordingService extends Service implements MediaRecorder.OnInfoLis
                         this
                 );
                 setLowQuality(mLowQuality);
+                setLongerDuration(mLongerDuration);
 
                 if (startRecording()) {
                     updateState(true);
@@ -459,6 +464,13 @@ public class RecordingService extends Service implements MediaRecorder.OnInfoLis
             getRecorder().setLowQuality(turnOn);
         }
     }
+
+    private void setLongerDuration(boolean longer) {
+        if (getRecorder() != null) {
+            getRecorder().setLongerDuration(longer);
+        }
+    }
+
 
     private void setStopDotVisible(boolean turnOn) {
         if (turnOn) {
