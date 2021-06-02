806296f5589a5b608c319f602615d07242ef56cf
Pranav Vashi <neobuddy89@gmail.com>
2021-06-02 20:08:04 +0530

    Allow setting custom symbol near text on charging [1/2]
    Thanks to @neobuddy89. I am just bringing it Q n R

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5985,6 +5985,12 @@ public final class Settings {
                 "status_bar_locked_on_secure_keyguard";
 
         /**
+         * Charging symbol near battery text percentage
+         * @hide
+         */
+        public static final String TEXT_CHARGING_SYMBOL = "text_charging_symbol";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
