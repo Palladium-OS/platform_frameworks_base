72e15bf1177fcd23adce06690a9b79a1e22d257a
703joko <703joko@gmail.com>
2021-07-22 12:39:13 +0700

    SystemUI : Introduce Battery Landscape | Left Battery Text [1/2]
    Inspiration from Ancient ROM, thanks to Kakek for helps
    
    Signed-off-by: 703joko <703joko@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4932,6 +4932,14 @@ public final class Settings {
         public static final String CUSTOM_AMBIENT_POCKETMODE_GESTURE = "custom_ambient_pocketmode_gesture";
 
         /**
+         * Setting to determine whether or not to show the battery percentage in the qs status bar header.
+         *    0 - Show remaining time
+         *    1 - Show percentage
+         * @hide
+         */
+        public static final String DO_LEFT_BATTERY_TEXT = "do_left_battery_text";
+
+        /**
          * Whether or not to enable multiple audio focus.
          * When enabled, requires more management by user over application playback activity,
          * for instance pausing media apps when another starts.
