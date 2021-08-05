c99392143fd538c923af6c704f044edbfe8f84cc
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-08-05 14:57:33 +0530

    PowerManagerService: Send broadcast when going to sleep
    Signed-off-by: Mukesh Singh <mks22584@gmail.com>

---
+++ core/java/com/android/internal/util/palladium/content/Intent.java
@@ -0,0 +1,15 @@
+package com.android.internal.util.palladium.content;
+import android.Manifest;
+
+/**
+ * crDroid specific intent definition class.
+ */
+public class Intent {
+
+    /**
+     * Broadcast action: go to sleep
+     * @hide
+     */
+    public static final String ACTION_GO_TO_SLEEP =
+            "palladium.intent.action.GO_TO_SLEEP";
+}
