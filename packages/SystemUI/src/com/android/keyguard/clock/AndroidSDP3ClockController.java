Evolution-X-AOSP/frameworks_base

SystemUI: Add time refresh capabilities for S clock controllers

packages/SystemUI/src/com/android/keyguard/clock/AndroidSDP3ClockController.java
@@ -367,6 +367,8 @@ public class AndroidSDP3ClockController implements ClockPlugin {
 
     @Override
     public void onTimeTick() {
+        mView.onTimeChanged();
+        mClock.refreshTime();
     }
 
     @Override

Palladium-OS/platform_frameworks_base

Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>
Change-Id: Iad4d1e8fd76b9a58e2926ac1265add69574662d8

packages/SystemUI/src/com/android/keyguard/clock/AndroidSDP3ClockController.java
@@ -367,6 +367,8 @@ public class AndroidSDP3ClockController implements ClockPlugin {
 
     @Override
     public void onTimeTick() {
+        mBigClockView.onTimeChanged();
+        mClock.refreshTime();
     }
 
     @Override