7e17ccf67a55518b5dd8bc78a55d83355e749a36
Lokesh773 <lokesh@palladiumos.com>
2021-09-12 00:04:24 +0530

    Revert "base: Bring back Gaming mode [1/3]"
    This reverts commit 3648c16ec8de8d4148c223bf97f67acbb4139195.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -530,8 +530,6 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     private PackageMonitor mPackageMonitor;
 
-    private boolean mHeadsUpDisabled, mGamingModeActivated;
-
     // XXX: gesture research
     private final GestureRecorder mGestureRec = DEBUG_GESTURES
         ? new GestureRecorder("/sdcard/statusbar_gestures.dat")
@@ -2320,12 +2318,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                     Settings.Secure.SYSUI_ROUNDED_FWVALS),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.GAMING_MODE_ACTIVE),
-                    false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.GAMING_MODE_HEADSUP_TOGGLE),
-                    false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_SHOW_TICKER),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
@@ -2395,7 +2387,6 @@ public class StatusBar extends SystemUI implements DemoMode,
             updateSwitchStyle();
             handleCutout(null);
             updateCorners();
-            setGamingMode();
             updateTickerAnimation();
             updateTickerTickDuration();
         }
@@ -2484,16 +2475,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (mKeyguardIndicationController != null) {
             mKeyguardIndicationController.updateChargingIndication();
         }
-    }
-
-    private void setGamingMode() {
-        mGamingModeActivated = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.GAMING_MODE_ACTIVE, 0,
-                UserHandle.USER_CURRENT) == 1;
-        mHeadsUpDisabled = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.GAMING_MODE_HEADSUP_TOGGLE, 1,
-                UserHandle.USER_CURRENT) == 1;
-        mNotificationInterruptStateProvider.setGamingPeekMode(mGamingModeActivated && mHeadsUpDisabled);
     }
 
     private void setScreenBrightnessMode() {
