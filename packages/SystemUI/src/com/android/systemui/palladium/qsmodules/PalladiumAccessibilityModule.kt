cc0b02d254100b0517e8bc9070e0453ca96c19a1
Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
2021-10-16 10:02:58 +0000

    base: Add Sound tile to Quick Settings
        abc ezio84: adapt to O, show volume panel with long press,
        use full dnd (no interruptions) as silent mode
    
        @beanstown106: Sound Tile improvements
        *added longpress action
        *added more descriptive labels for each state instead of always saying sound
    
        ezio84: Adapt for N
        Change-Id: I46f4f8cc62683144a9bd714ca67e2a0f46940d96
    
    Squashed commit of the following:
    
    commit 69f86d9a38b5a942e3f9c914895ee45c0079e8e9
    Author: maxwen <max.weninger@gmail.com>
    Date:   Fri Apr 13 03:32:51 2018 +0200
    
        base: SystemUI: stop crashing qs tiles during boot
    
        this triggers my ocd
        continues a579c2430f1a0bf088023b4283dcec37776c070d
    
        Change-Id: I31719b595cb50a330f26e25074243e04c06f63c1
        Signed-off-by: xyyx <xyyx@mail.ru>
    
    commit f419619c033fa99a809f53aa82dbdd7d6732177c
    Author: xyyx <xyyx@mail.ru>
    Date:   Thu Sep 28 11:55:36 2017 +0800
    
        SoundTile: Change ZEN_MODE_NO_INTERRUPTIONS to ZEN_MODE_ALARMS
    
        Change-Id: I5edaaa4d551630049b29e09d3cecfb5ffd503e99
    
    commit 916645271f57c47d60bb0a04c4e51776b51a18f1
    Author: Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
    Date:   Sun Sep 18 22:33:08 2016 +0200
    
        Add Sound tile to Quick Settings
    
        abc ezio84: adapt to O, show volume panel with long press,
        use full dnd (no interruptions) as silent mode
    
        @beanstown106: Sound Tile improvements
        *added longpress action
        *added more descriptive labels for each state instead of always saying sound
    
        ezio84: Adapt for N
        Change-Id: I46f4f8cc62683144a9bd714ca67e2a0f46940d96
    
    Change-Id: I178bfa69ff181f0f65f94ffae1444aaea5f21a80
    
    -----
    
    SystemUI: Update SoundTile
    
    Make the tile active only in a state different than its
    "normal" state (ring). When it's in normal state, no need
    to draw any attention to it.
    
    - SOS edits:
    Make the default sound state also active as the footer has on/off string so even when it's sound mode it should show on instead of off
    
    Change-Id: I20c20c7c2a05ebe92e887c023e792ad47ad5bbfa
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    
    -----
    
    SystemUI: QS: SoundTile: Don't toggle DND
    
    * Allow Sound tile to toggle "Mute" instead of "DND" since we've DND Tile for DND work.
    
    Change-Id: I3a1b03c9d1763602ed436233ba77045d35fd700c
    Co-authored-by: Ali B <abittin@gmail.com>
    Co-authored-by: AnkitGourav <Ankitgourav035@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    
    ----------------------
    
    Adapt for android 13 qpr3 release
    
    Co-authored-by: sanjheevi <sanjheevisv@gmail.com>

--- packages/SystemUI/src/com/android/systemui/palladium/qsmodules/PalladiumAccessibilityModule.kt
+++ packages/SystemUI/src/com/android/systemui/palladium/qsmodules/PalladiumAccessibilityModule.kt
@@ -19,6 +19,7 @@ package com.android.systemui.palladium.qsmodules
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.SyncTile
+import com.android.systemui.qs.tiles.SoundTile
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -38,4 +39,10 @@ interface PalladiumAccessibilityModule {
     @IntoMap
     @StringKey(SyncTile.TILE_SPEC)
     fun bindSyncTile(syncTile: SyncTile): QSTileImpl<*>     
+
+    /** Inject SoundTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(SoundTile.TILE_SPEC)
+    fun bindSoundTile(soundTile: SoundTile): QSTileImpl<*>    
 }
