c9b1f13e6da33f8107a3bccc57b7b29080815745
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 08:31:51 +0530

    SystemUI: integrate smartspace and backgesture provider
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I6cf1e5147b502a0596e9cb2cc88eaefbd9487b50
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- packages/SystemUI/src/com/palladium/android/systemui/SystemUIPalladiumFactory.java
+++ packages/SystemUI/src/com/palladium/android/systemui/SystemUIPalladiumFactory.java
@@ -1,12 +1,22 @@
 package com.palladium.android.systemui;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Handler;
+
+import com.google.android.systemui.gesture.BackGestureTfClassifierProviderGoogle;
 
 import com.palladium.android.systemui.dagger.DaggerGlobalRootComponentPalladium;
 import com.palladium.android.systemui.dagger.GlobalRootComponentPalladium;
+import com.palladium.android.systemui.dagger.SysUIComponentPalladium;
 
 import com.android.systemui.SystemUIFactory;
 import com.android.systemui.dagger.GlobalRootComponent;
+import com.android.systemui.navigationbar.gestural.BackGestureTfClassifierProvider;
+import com.android.systemui.screenshot.ScreenshotNotificationSmartActionsProvider;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class SystemUIPalladiumFactory extends SystemUIFactory {
     @Override
@@ -14,5 +24,18 @@ public class SystemUIPalladiumFactory extends SystemUIFactory {
         return DaggerGlobalRootComponentPalladium.builder()
                 .context(context)
                 .build();
+    }
+
+    @Override
+    public BackGestureTfClassifierProvider createBackGestureTfClassifierProvider(AssetManager am, String modelName) {
+        return new BackGestureTfClassifierProviderGoogle(am, modelName);
+    }
+
+    @Override
+    public void init(Context context, boolean fromTest) throws ExecutionException, InterruptedException {
+        super.init(context, fromTest);
+        if (shouldInitializeComponents()) {
+            ((SysUIComponentPalladium) getSysUIComponent()).createKeyguardSmartspaceController();
+        }
     }
 }
