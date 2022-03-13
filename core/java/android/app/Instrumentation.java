yaap-surya/frameworks_base

core: Make build property spoofing more reliable
Determining the package name from Zygote's post-fork process name is
unreliable for several reasons:
  - If USAP is enabled, Zygote will fork with the USAP process name, and
    spoofing will never trigger when the forked process specializes into
    a specific app.
  - With the native fork loop backported from AOSP master,
    Zygote.setAppProcessName() is no longer used for most apps because
    the forking is handled by native code.

Instrumentation#newApplication() is responsible for creating the
Application singleton during each app's initialization, so hook it
instead for a more reliable and robust trigger.

core/java/android/app/Instrumentation.java
@@ -59,6 +59,8 @@ import com.android.internal.content.ReferrerIntent;
 
 import com.android.internal.gmscompat.GmsHooks;
 
+import com.android.internal.util.yaap.PixelPropsUtils;
+
 import java.io.File;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -1179,6 +1181,7 @@ public class Instrumentation {
                 .instantiateApplication(cl, className);
         app.attach(context);
         GmsHooks.initApplicationBeforeOnCreate(app);
+        PixelPropsUtils.setProps(app.getPackageName());
         return app;
     }
     
@@ -1197,6 +1200,7 @@ public class Instrumentation {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
         GmsHooks.initApplicationBeforeOnCreate(app);
+        PixelPropsUtils.setProps(app.getPackageName());
         return app;
     }
 

Palladium-OS/platform_frameworks_base

Change-Id: I94c7ffd5d832637704ca196b4af7b9937e7204d9

core/java/android/app/Instrumentation.java
@@ -59,6 +59,8 @@ import com.android.internal.content.ReferrerIntent;
 
 import com.android.internal.gmscompat.GmsHooks;
 
+import com.android.internal.util.custom.PixelPropsUtils;
+
 import java.io.File;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -1195,6 +1197,7 @@ public class Instrumentation {
         String packageName = app.getPackageName();
         PixelPropsUtils.setProps(packageName);
         GmsHooks.initApplicationBeforeOnCreate(app);
+        PixelPropsUtils.setProps(app.getPackageName());
         return app;
     }
     
@@ -1215,6 +1218,7 @@ public class Instrumentation {
         String packageName = app.getPackageName();
         PixelPropsUtils.setProps(packageName);
         GmsHooks.initApplicationBeforeOnCreate(app);
+        PixelPropsUtils.setProps(app.getPackageName());
         return app;
     }
 