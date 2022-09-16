53acf90a5509fa9dac25af46564f750fd32f6a25
jhenrique09 <jhsv09@gmail.com>
2022-09-16 15:32:41 +0700

    Smartspace: Fix music indication not showing on doze/aod
    [jhonboy121]: use the constant value defined in SmartSpaceTarget for feature type
    
    Change-Id: If375051a6bf44c67344ad05a4366b6cc663f15d9
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SystemUI/src/com/google/android/systemui/smartspace/KeyguardMediaViewController.java
+++ packages/SystemUI/src/com/google/android/systemui/smartspace/KeyguardMediaViewController.java
@@ -153,7 +153,7 @@ public final class KeyguardMediaViewController {
                                     "deviceMedia",
                                     mediaComponent,
                                     UserHandle.of(userTracker.getUserId()))
-                            .setFeatureType(41)
+                            .setFeatureType(SmartspaceTarget.FEATURE_MEDIA)
                             .setHeaderAction(build)
                             .build();
             BcSmartspaceDataPlugin.SmartspaceView smartspaceView = getSmartspaceView();
